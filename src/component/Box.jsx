cdd0d2bfc9e469b4b8b381988541b6743d001069
hijiki <22rd149@ms.dendai.ac.jp>
2024-05-16 23:36:22 +0900

    box,topmain change

--- src/component/Box.jsx
+++ src/component/Box.jsx
@@ -1,7 +1,9 @@
 // トップ画面の箱
 
+import React from 'react';
 import PropTypes from 'prop-types'; // 型定義
 import { Link } from 'react-router-dom'; // ページ遷移用タグ
+let hako = "/src/assets/box_brown.png";
 
 Box.propTypes = { // props増やしたら型定義しておく
   name: PropTypes.string,
@@ -10,8 +12,11 @@ Box.propTypes = { // props増やしたら型定義しておく
 
 function Box({ name, url }) {
   return (
-    <div>
-      <Link to={`/123456/${url}`}>{`${name}用`}</Link>
+    <div className="m-4 flex flex-col flex-wrap items-center">
+      <Link to={`/applink/${url}`}>
+        <img src={hako} alt={`${name}用の画像`} style={{ width: '470px', height: '400px' }} />
+      </Link>
+        <div className="text-center mt-2 font-semibold text-xl">{`${name}を確認できるよ！`}</div>
     </div>
   )
 }
--- src/component/TopMain.jsx
+++ src/component/TopMain.jsx
@@ -1,12 +1,13 @@
 // トップ画面 main
-
-import Box from "./Box"
+import React from 'react';
+import Box from "./Box";
+let haikei = "/src/assets/haikei.png";
 
 function TopMain() {
     return (
-      <div>
-        <Box name = "個人" url = "personal" />
-        <Box name = "グループ" url = "group" />
+      <div class = "flex flex-wrap justify-around items-center min-h-screen bg-cover bg-center" style={{ backgroundImage: `url(${haikei})` }}>
+        <Box name = "自分の目標" url = "personal" />
+        <Box name = "みんなの目標" url = "group" />
       </div>
     )
   }
