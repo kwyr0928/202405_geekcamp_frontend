a279565bbcf68853f6ac245e3d81aed8c63ebb1c
23suzuki <23rd099@ms.dendai.ac.jp>
2024-05-18 17:12:30 +0900

    Goal

--- src/component/Goal.jsx
+++ src/component/Goal.jsx
@@ -3,6 +3,8 @@
 
 import React from 'react'; 
 import PropTypes from 'prop-types'; // 型定義
+import axios from 'axios';
+let haikei = "/src/assets/haikei.png";
 let pic = "/src/assets/bakudan.png";
 
 Goal.propTypes = {  // props増やしたら型定義しておく
@@ -13,13 +15,40 @@ Goal.propTypes = {  // props増やしたら型定義しておく
 };
 
 function Goal({ name, description, start_date, end_date }) {
+
+  const handleClick = () => {
+    // axios.post('/status', {
+    //   name: name,
+    //   goal: description
+    // })
+    // .then(function (response) {
+    //   console.log(response);
+    // })
+    // .catch(function (error) {
+    //   console.log(error);
+    // });
+  };
+
+
   return (
-      <div class="box">
-        <img src={pic} alt={"爆弾"} style={{ width: '200px', height: '200px' }} />
-        <h1>{`名前：${name}`}</h1>
-        <h2>{`目標：${description}`}</h2>
-        <h3>{`開始：${start_date}`}</h3>
-        <h3>{`締切：${end_date}`}</h3>
+    <div className= "flex flex-wrap justify-start items-start min-h-screen bg-cover bg-center p-20">
+      <div className="bg-white p-7 rounded-lg relative flex justify-center items-center">
+        <img src={pic} className="ml-28 mt-2 mb-8" alt={"爆弾"} style={{ width: '315px', height: '234px' } } />
+        <div className="text-left mt-2 text-base absolute bottom-9 left-9">
+          <div className="my-10 text-2xl  font-black	">
+            <h1>{`名前：${name}`}</h1>
+          </div>
+          <div className="mb-8 text-2xl font-black">
+            <h2>{`目標：${description}`}</h2>
+          </div>
+          <div className="mb-3 text-sm text-center p-2 bg-white md:w-1/2">
+            <h3>{`作成日：${start_date}`}</h3>
+          </div>
+          <div className="animate-vibrate-1 text-sm text-center p-2 bg-red-500 md:w-1/2 ">
+            <button onClick={handleClick}>達成した！</button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
