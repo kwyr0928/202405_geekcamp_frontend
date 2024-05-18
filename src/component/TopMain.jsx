90277ed764ce523ac31779d28b75203cee2dc7c6
23suzuki <23rd099@ms.dendai.ac.jp>
2024-05-18 17:20:51 +0900

    TopMain

--- src/component/TopMain.jsx
+++ src/component/TopMain.jsx
@@ -5,7 +5,7 @@ let haikei = "/src/assets/haikei.png";
 
 function TopMain() {
     return (
-      <div class = "flex flex-wrap justify-around items-center min-h-screen bg-cover bg-center" style={{ backgroundImage: `url(${haikei})` }}>
+      <div className = "flex flex-wrap justify-around items-center min-h-screen bg-cover bg-center" style={{ backgroundImage: `url(${haikei})` }}>
         <Box name = "自分の目標" url = "personal" />
         <Box name = "みんなの目標" url = "group" />
       </div>
